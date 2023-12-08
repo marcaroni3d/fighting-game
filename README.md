89449ceffad974356c3973d776c5e242e13a3a5c
marcaroni3d <marc.eddington@gmail.com>
2023-12-08 17:36:03 -0500

    version 1.0

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# fighting-game
+# Fighting Game
+
+A in-browser fighting game built with Javascript.
+
+[Live Demo](https://marcaroni3d.github.io/fighting-game/)
---
+++ index.js
@@ -0,0 +1,151 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const gravity = 0.2
+const velocityRight = 5
+const velocityLeft = -5
+const velocityJump = -10
+
+class Sprite {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 150
+        this.width = 50
+        this.lastKey
+    }
+
+    draw() {
+        c.fillStyle = 'red'
+        c.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    update() {
+        this.draw()
+
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
+    }
+}
+
+const player = new Sprite({
+    position: {
+        x: 0,
+        y: 0
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const enemy = new Sprite({
+    position: {
+        x: 400,
+        y: 100
+    },
+    velocity: {
+        x: 0,
+        y: 0
+    }
+})
+
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    ArrowRight: {
+        pressed: false
+    },
+    ArrowLeft: {
+        pressed: false
+    }
+}
+
+function animate() {
+    window.requestAnimationFrame(animate)
+    c.fillStyle = 'black'
+    c.fillRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    enemy.update()
+
+    player.velocity.x = 0
+    enemy.velocity.x = 0
+
+    // player movement
+    if (keys.a.pressed && player.lastKey === 'a') {
+        player.velocity.x = velocityLeft
+    } else if (keys.d.pressed && player.lastKey === 'd') {
+        player.velocity.x = velocityRight
+    }
+
+    // enemy movement
+    if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+        enemy.velocity.x = velocityLeft
+    } else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+        enemy.velocity.x = velocityRight
+    }
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        // player movement
+        case 'd':
+            keys.d.pressed = true
+            player.lastKey = 'd'
+            break
+        case 'a':
+            keys.a.pressed = true
+            player.lastKey = 'a'
+            break
+        case 'w':
+            player.velocity.y = velocityJump
+            break
+
+        // enemy movement
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = true
+            enemy.lastKey = 'ArrowRight'
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = true
+            enemy.lastKey = 'ArrowLeft'
+            break
+        case 'ArrowUp':
+            enemy.velocity.y = velocityJump
+            break
+    }
+})
+
+window.addEventListener('keyup', (event) => {
+    switch (event.key) {
+        // player movement
+        case 'd':
+            keys.d.pressed = false
+            break
+        case 'a':
+            keys.a.pressed = false
+            break
+    
+        // enemy movement
+        case 'ArrowRight':
+            keys.ArrowRight.pressed = false
+            break
+        case 'ArrowLeft':
+            keys.ArrowLeft.pressed = false
+    }
+})
